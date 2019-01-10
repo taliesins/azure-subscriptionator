1a76f89b8c2664530de05798672bc45c14f58b4d
Taliesin Sisson <taliesins@yahoo.com>
2019-01-10 12:48:50 +0000

    Format json for policies and policy sets during build process. Order is important that is why we need to format the json before using it

--- build.ps1
+++ build.ps1
@@ -1,27 +1,52 @@
+$azureDefinitionManagementGroup='Bob'
+
+function Format-Json([Parameter(Mandatory, ValueFromPipeline)][String] $json) {
+    $indent = 0;
+    ($json -replace "\\u003c","<" -replace "\\u003e",">" -replace "\\u0027","'" -Split '\n' |
+      % {
+        if ($_ -match '[\}\]]') {
+          # This line contains  ] or }, decrement the indentation level
+          $indent--
+        }
+        $line = (' ' * $indent * 2) + $_.TrimStart().Replace(':  ', ': ')
+        if ($_ -match '[\{\[]') {
+          # This line contains [ or {, increment the indentation level
+          $indent++
+        }
+        $line
+    }) -Join "`n"
+}
+
 $azurePolicyPaths = Get-ChildItem PolicyDefinitions | ?{ $_.PSIsContainer } | %{Join-Path -Path $_.FullName -ChildPath 'azurepolicy.json'} | ?{ Test-Path $_}
 
 $azurePolicyPaths | %{
-    $azurePolicyJson = [System.IO.File]::ReadAllLines($_) | ConvertFrom-Json
+    $azurePolicyPath = $_
+    $azurePolicy = [System.IO.File]::ReadAllLines($azurePolicyPath) | ConvertFrom-Json
+    $azurePolicyJson = $azurePolicy | ConvertTo-Json -Depth 99 | Format-Json
+    [System.IO.File]::WriteAllLines($azurePolicyPath, $azurePolicyJson)
 
-    $azurePolicyParameterPath = Join-Path -Path (Split-Path $_ -Parent) -ChildPath 'azurepolicy.parameters.json'  
-    $azurePolicyParameterJson = $azurePolicyJson.properties.parameters | ConvertTo-Json -Depth 99
+    $azurePolicyParameterPath = Join-Path -Path (Split-Path $azurePolicyPath -Parent) -ChildPath 'azurepolicy.parameters.json'  
+    $azurePolicyParameterJson = $azurePolicy.properties.parameters | ConvertTo-Json -Depth 99 | Format-Json
     [System.IO.File]::WriteAllLines($azurePolicyParameterPath, $azurePolicyParameterJson)
 
-    $azurePolicyPolicyRulePath = Join-Path -Path (Split-Path $_ -Parent) -ChildPath 'azurepolicy.rules.json'
-    $azurePolicyPolicyRuleJson =  $azurePolicyJson.properties.policyRule | ConvertTo-Json -Depth 99
+    $azurePolicyPolicyRulePath = Join-Path -Path (Split-Path $azurePolicyPath -Parent) -ChildPath 'azurepolicy.rules.json'
+    $azurePolicyPolicyRuleJson =  $azurePolicy.properties.policyRule | ConvertTo-Json -Depth 99 | Format-Json
     [System.IO.File]::WriteAllLines($azurePolicyPolicyRulePath, $azurePolicyPolicyRuleJson)
 }
 
 $azurePolicySetPaths = Get-ChildItem PolicySetDefinitions | ?{ $_.PSIsContainer } | %{Join-Path -Path $_.FullName -ChildPath 'azurepolicyset.json'} | ?{ Test-Path $_}
 
 $azurePolicySetPaths | %{
-    $azurePolicySetJson = [System.IO.File]::ReadAllLines($_) | ConvertFrom-Json
+    $azurePolicySetPath = $_
+    $azurePolicySet = [System.IO.File]::ReadAllLines($azurePolicySetPath) | ConvertFrom-Json
+    $azurePolicySetJson = $azurePolicySet | ConvertTo-Json -Depth 99 | Format-Json
+    [System.IO.File]::WriteAllLines($azurePolicySetPath, $azurePolicySetJson)
 
-    $azurePolicyParameterPath = Join-Path -Path (Split-Path $_ -Parent) -ChildPath 'azurepolicyset.parameters.json'  
-    $azurePolicyParameterJson = $azurePolicySetJson.properties.parameters | ConvertTo-Json -Depth 99
+    $azurePolicyParameterPath = Join-Path -Path (Split-Path $azurePolicySetPath -Parent) -ChildPath 'azurepolicyset.parameters.json'  
+    $azurePolicyParameterJson = $azurePolicySet.properties.parameters | ConvertTo-Json -Depth 99 | Format-Json
     [System.IO.File]::WriteAllLines($azurePolicyParameterPath, $azurePolicyParameterJson)
 
-    $azurePolicyPolicyDefinitionPath = Join-Path -Path (Split-Path $_ -Parent) -ChildPath 'azurepolicyset.definitions.json'
-    $azurePolicyPolicyDefinitionJson =  $azurePolicySetJson.properties.policyDefinitions | ConvertTo-Json -Depth 99
+    $azurePolicyPolicyDefinitionPath = Join-Path -Path (Split-Path $azurePolicySetPath -Parent) -ChildPath 'azurepolicyset.definitions.json'
+    $azurePolicyPolicyDefinitionJson =  $azurePolicySet.properties.policyDefinitions | ConvertTo-Json -Depth 99 | Format-Json
     [System.IO.File]::WriteAllLines($azurePolicyPolicyDefinitionPath, $azurePolicyPolicyDefinitionJson)
 }
